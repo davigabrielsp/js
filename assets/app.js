be4c5cbaa919a475d08fd5c5bfe14412ecc6a75f
davigabrielsp <davigabriel.sp@outlook.com>
2025-03-24 20:01:08 -0300

    functions

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+document.getElementById('texto').innerHTML = "ola mundo";
+document.getElementById('read').innerHTML = "lendo texto";
+
+
+
+
+function calcularDados(a,b,c)
+{
+    return soma = (a + b) /c;
+}
+
+calcularDados(10,20,30);
